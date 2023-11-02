6794f58ea700ac69b4ffe079687ff6b7791b5ef7
ftoledo <franciscojaviertoledocollante@gmail.com>
2023-11-02 16:37:56 -0300

    first commit

---
+++ SistemaInventarioV7.Modelos/Bodega.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventarioV7.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required (ErrorMessage = "Nombre es Requerido")]
+        [MaxLength(60, ErrorMessage ="Nombre debe ser maximo 60 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Descripción es Requerida")]
+        [MaxLength(100, ErrorMessage = "Descripción debe ser maximo 60 caracteres")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage = "Estado es Requerido")]
+        public bool Estado { get; set; }
+    }
+}
