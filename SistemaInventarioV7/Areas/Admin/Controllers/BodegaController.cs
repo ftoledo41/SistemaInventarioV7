c09c8d16f66cf2cb42ad050bf617c27d1e9d4367
ftoledo <franciscojaviertoledocollante@gmail.com>
2023-11-03 18:17:37 -0300

    CRUD de Bodegas

---
+++ SistemaInventarioV7/Areas/Admin/Controllers/BodegaController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+using SistemaInventarioV7.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventarioV7.Modelos;
+using SistemaInventarioV7.Utilidades;
+
+namespace SistemaInventarioV7.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BodegaController : Controller
+    {
+        //Con esto ya podemos utilizar todo lo que envuelve a la unidad de trabajo
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public BodegaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //? es por si es nulo es un registro nuevo; caso contrario lo actualiza
+        public async Task<IActionResult> UpSert(int? id)
+        {
+            Bodega bodega = new Bodega();
+            if(id == null) 
+            {
+                bodega.Estado = true;
+                return View(bodega);
+            }
+            bodega = await _unidadTrabajo.Bodega.Obtener(id.GetValueOrDefault());
+            if(bodega == null)
+            {
+                return NotFound();
+            }
+            return View(bodega);            
+        }
+
+        [HttpPost]
+        //ValidateAntiForgeryToken Sirve para la evitar la falsificacion de solicitudes de un sitio de
+        //otra pagina que intenta cargar registros en nuestra pagina
+        [ValidateAntiForgeryToken] 
+        public async Task<IActionResult> Upsert(Bodega bodega)
+        {
+            if (ModelState.IsValid)
+            {
+                if(bodega.Id == 0)
+                {
+                    await _unidadTrabajo.Bodega.Agregar(bodega);
+                    TempData[DS.Exitosa] = "Bodega creada exitosamente";
+                }
+                else
+                {
+                    _unidadTrabajo.Bodega.Actualizar(bodega);
+                    TempData[DS.Exitosa] = "Bodega actualizada exitosamente";
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            TempData[DS.Error] = "Error al grabar Bodega";
+            return View(bodega);
+        }
+
+        #region API
+        //Es API porque llamamos a algo externo para realizar la accion, en este caso, llamamos a código JavaScript
+        [HttpGet]
+        // IActionResult devuelve tambien objetos de tipo json
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Bodega.ObtenerTodos();
+            //data guardara todo lo obtenido
+            return Json(new { data = todos });
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bodegaDb = await _unidadTrabajo.Bodega.Obtener(id);
+            if(bodegaDb == null)
+            {
+                return Json(new { success = false, message = "Error al borrar Bodega" });
+            }
+            _unidadTrabajo.Bodega.Remover(bodegaDb);
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Bodega borrada exitosamente" });
+        }
+
+        [ActionName("ValidarNombre")]
+        public async Task<IActionResult> ValidarNombre(string nombre, int id=0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Bodega.ObtenerTodos();
+            if (id == 0)
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+            }
+                
+            else
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim() && b.Id != id);
+            }                
+
+            if (valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new { data = false });
+        }
+
+        #endregion
+    }
+}
