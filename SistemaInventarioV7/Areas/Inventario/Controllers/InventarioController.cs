27b41611256075a3c613c928e6dbe9a1c3a24b94
ftoledo <franciscojaviertoledocollante@gmail.com>
2023-11-07 17:35:33 -0300

    Generar Reportes con Rotativa

--- SistemaInventarioV7/Areas/Inventario/Controllers/InventarioController.cs
+++ SistemaInventarioV7/Areas/Inventario/Controllers/InventarioController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
 using SistemaInventarioV7.AccesoDatos.Repositorio.IRepositorio;
 using SistemaInventarioV7.Modelos;
 using SistemaInventarioV7.Modelos.ViewModels;
@@ -213,6 +214,32 @@ namespace SistemaInventarioV7.Areas.Inventario.Controllers
                 );
 
             return View(kardexInventarioVM);
+        }
+
+        public async Task<IActionResult> ImprimirKardex(string fechaInicio, string fechaFinal, int productoId)
+        {
+            KardexInventarioVM kardexInventarioVM = new KardexInventarioVM();
+            kardexInventarioVM.Producto = new Producto();
+            kardexInventarioVM.Producto = await _unidadTrabajo.Producto.Obtener(productoId);
+
+            kardexInventarioVM.FechaInicio = DateTime.Parse(fechaInicio);
+            kardexInventarioVM.FechaFinal = DateTime.Parse(fechaFinal);
+
+            kardexInventarioVM.KardexInventarioLista = await _unidadTrabajo.KardexInventario.ObtenerTodos(
+                                                                   k => k.BodegaProducto.ProductoId == productoId &&
+                                                                       (k.FechaRegistro >= kardexInventarioVM.FechaInicio &&
+                                                                        k.FechaRegistro <= kardexInventarioVM.FechaFinal),
+                                            IncluirPropiedades: "BodegaProducto,BodegaProducto.Producto,BodegaProducto.Bodega",
+                                            orderBY: o => o.OrderBy(o => o.FechaRegistro)
+                );
+
+            return new ViewAsPdf("ImprimirKardex", kardexInventarioVM)
+            {
+                FileName = "KardexProducto.pdf",
+                PageOrientation = Rotativa.AspNetCore.Options.Orientation.Portrait,
+                PageSize = Rotativa.AspNetCore.Options.Size.A4,
+                CustomSwitches = "--page-offset 0 --footer-center [page] --footer-font-size 12"
+            };
         }
 
         #region API
--- SistemaInventarioV7/Program.cs
+++ SistemaInventarioV7/Program.cs
@@ -76,4 +76,7 @@ app.MapControllerRoute(
     pattern: "{area=Inventario}/{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
+IWebHostEnvironment env = app.Environment;
+Rotativa.AspNetCore.RotativaConfiguration.Setup(env.WebRootPath, "..\\Rotativa\\Windows\\");
+
 app.Run();
